e1089275ae3756aa0caa0d44c9413e6f931cdfc6
马熙涵 <xihanma370@gmail.com>
2025-05-07 21:36:30 -0400

    improved authetication logic

--- cmd/liteproxy/main.go
+++ cmd/liteproxy/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+
+	socks5 "github.com/Icannotcode0/LiteProxy/pkg/socks5"
+)
+
 func main() {
+
+	classicSocks5Server := socks5.ClassicSock5Server()
+
+	if err := classicSocks5Server.ListenAndServe(); err != nil {
+
+		log.Fatalf("[LiteProxy] Critical Error in SOCKS5 Server: %v", err)
+	}
 
 }
