58811cf3e336b8214779a2d4a739d8e1ac75b5b7
Icannotcode0 <xihanma370@gmail.com>
2025-05-08 15:00:03 -0400

    added json file loading logic

--- cmd/liteproxy/main.go
+++ cmd/liteproxy/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"log"
+	//"github.com/Icannotcode0/LiteProxy/internal/config"
+	"github.com/Icannotcode0/LiteProxy/internal/config"
+	"github.com/Icannotcode0/LiteProxy/pkg/socks5"
 
-	socks5 "github.com/Icannotcode0/LiteProxy/pkg/socks5"
+	//socks5 "github.com/Icannotcode0/LiteProxy/pkg/socks5"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
-	classicSocks5Server := socks5.ClassicSock5Server()
+	//classicSocks5Server := socks5.ClassicSock5Server()
 
-	if err := classicSocks5Server.ListenAndServe(); err != nil {
+	serverConfig, err := config.LoadJSON("/Users/maxihan/Desktop/LiteProxy/configs/socks5-server-config.json")
+	if err != nil {
+		logrus.Errorf("[LiteProxy] Unable to Load JSON file: %v", err)
+		return
+	}
 
-		log.Fatalf("[LiteProxy] Critical Error in SOCKS5 Server: %v", err)
+	newSocks5Server, err := socks5.NewSocks5Server(serverConfig)
+	if err != nil {
+		logrus.Fatalf("[LiteProxy] Cannot Initialize New SOCKS5 Server: %v", err)
+	}
+
+	if err := newSocks5Server.ListenAndServe(); err != nil {
+
+		logrus.Fatalf("[LiteProxy] Critical Error in SOCKS5 Server: %v", err)
 	}
 
 }
